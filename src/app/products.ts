5575e526ba076e06cb6089ba6416d33972387347
Lindsay902 <lindsaymlee88@gmail.com>
2020-11-07 17:08:55 -0600

    updated products page with photos

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,16 +1,19 @@
 export const products = [
     {
       name: 'Gingerbread',
+      image: './assets/gingerbreadcookies.jpg',
       price: 10,
       description: 'One dozen soft gingerbread cookies with molasses and spices.'
     },
     {
       name: 'Sugar Cookies',
+      image: './assets/sugarcookies.jpg',
       price: 9,
       description: 'One dozen decorated sugar cookies.'
     },
     {
       name: 'Chocolate Chip',
+      image: './assets/chocolatechip.jpg',
       price: 8,
       description: 'One dozen soft and gooey chocolate chip cookies.'
     }
