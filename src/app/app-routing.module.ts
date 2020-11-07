e8ce66ba4f8335e0fda1eae847713967702db038
Lindsay902 <lindsaymlee88@gmail.com>
2020-11-06 22:24:26 -0600

    added all links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductsComponent } from './products/products.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component'
+
+
+
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent},
+  { path: 'home', component: AppComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { ProductsComponent } from './products/products.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    NavigationBarComponent,    
+    NavigationBarComponent,
+    ContactComponent,    
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     RouterModule.forRoot([
       {path: 'products', component: ProductsComponent},
+      {path: 'home', component: AppComponent},
     ]),
   ],
   providers: [],
